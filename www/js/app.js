594224db1cc55535f840e97e0a4bbe91cc66e748
grandgeorg <viktor@grandgeorg.de>
2022-10-24 19:05:51 +0200

    add sort controlls to tag cloud

--- www/js/app.js
+++ www/js/app.js
@@ -21,7 +21,8 @@
       selectedTags: [],
       tagCloud: {
         tags: [],
-      }
+      },
+      pageCards: []
     };
 
     const elMenuToggle = document.querySelector(".burger");
@@ -424,6 +425,12 @@
           const modalBodyContent = document.createElement('div');
           modalBodyContent.setAttribute('class', 'modal-body-content');
 
+          // tagcloud header
+          const tagCloudHeader = document.createElement('div');
+          tagCloudHeader.setAttribute('class', 'tag-cloud-header');
+
+
+          // tagcloud filter
           const tagCloudFilter = document.createElement('div');
           tagCloudFilter.setAttribute('class', 'tag-cloud-filter');
 
@@ -458,7 +465,88 @@
 
           tagCloudFilter.appendChild(tagCloudFilterLabel);
           tagCloudFilter.appendChild(tagCloudFilterInput);
+          tagCloudHeader.appendChild(tagCloudFilter);
 
+          // tagcloud sort
+          const tagCloudSort = document.createElement('div');
+          tagCloudSort.setAttribute('class', 'tag-cloud-sort');
+
+          const tagCloudSortLabel = document.createElement('label');
+          tagCloudSortLabel.setAttribute('for', 'tag-cloud-sort-select');
+          tagCloudSortLabel.setAttribute('class', 'sort-label');
+          tagCloudSortLabel.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+              <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+              <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+            </svg><span class="description">sort by</span>`;
+
+
+          const tagCloudOrderSelect = document.createElement('select');
+          tagCloudOrderSelect.setAttribute('id', 'tag-cloud-sort-select');
+          tagCloudOrderSelect.setAttribute('class', 'sort-select');
+          tagCloudOrderSelect.innerHTML = `
+            <option value="name">name</option>
+            <option value="count">count</option>`;
+            tagCloudOrderSelect.addEventListener('change', (event) => {
+            // const sortValue = event.target.value;
+            // const tagCloud = document.querySelector('.tag-cloud');
+            // const tagCloudItems = tagCloud.querySelectorAll('.tag-cloud-item');
+            // const tagCloudItemsArray = Array.from(tagCloudItems);
+            // const sortedTagCloudItems = tagCloudItemsArray.sort((a, b) => {
+            //   if (sortValue === 'name') {
+            //     return a.innerText.localeCompare(b.innerText);
+            //   } else if (sortValue === 'count') {
+            //     return b.dataset.count - a.dataset.count;
+            //   }
+            // });
+          });
+
+          const tagCloudOrderLabel = document.createElement('label');
+          tagCloudOrderLabel.setAttribute('for', 'tag-cloud-sort-order');
+          tagCloudOrderLabel.setAttribute('class', 'order-label');
+          tagCloudOrderLabel.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-alpha-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371h-1.781zm1.57-.785L11 2.687h-.047l-.652 2.157h1.351z"/>
+              <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V14zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"/>
+            </svg><span class="description">order</span>`;
+
+          const tagCloudSortOrder = document.createElement('select');
+          tagCloudSortOrder.setAttribute('id', 'tag-cloud-sort-order');
+          tagCloudSortOrder.setAttribute('class', 'order-select');
+          tagCloudSortOrder.innerHTML = `
+            <option value="asc">ascending</option>
+            <option value="desc">descending</option>`;
+          tagCloudSortOrder.addEventListener('change', (event) => {
+            // const sortOrder = event.target.value;
+            // const tagCloud = document.querySelector('.tag-cloud');
+            // const tagCloudItems = tagCloud.querySelectorAll('.tag-cloud-item');
+            // const tagCloudItemsArray = Array.from(tagCloudItems);
+            // const sortedTagCloudItems = tagCloudItemsArray.sort((a, b) => {
+            //   if (sortOrder === 'asc') {
+            //     return a.innerText.localeCompare(b.innerText);
+            //   } else if (sortOrder === 'desc') {
+            //     return b.innerText.localeCompare(a.innerText);
+            //   }
+            // });
+          });
+
+          const inputGroupSort = document.createElement('div');
+          inputGroupSort.setAttribute('class', 'input-group');
+          inputGroupSort.appendChild(tagCloudSortLabel);
+          inputGroupSort.appendChild(tagCloudOrderSelect);
+
+          const inputGroupOrder = document.createElement('div');
+          inputGroupOrder.setAttribute('class', 'input-group');
+          inputGroupOrder.appendChild(tagCloudOrderLabel);
+          inputGroupOrder.appendChild(tagCloudSortOrder);
+
+          tagCloudSort.appendChild(inputGroupSort);
+          tagCloudSort.appendChild(inputGroupOrder);
+
+          tagCloudHeader.appendChild(tagCloudSort);
+
+
+          // tagcloud
           const tagCloud = document.createElement('div');
           tagCloud.setAttribute('class', 'tag-cloud');
 
@@ -506,6 +594,46 @@
             tagCloud.appendChild(tagButton);
           });
 
+          // -------------
+
+
+          // page filter
+          const pageFilter = document.createElement('div');
+          pageFilter.setAttribute('class', 'page-filter');
+
+          const pageFilterLabel = document.createElement('label');
+          pageFilterLabel.setAttribute('for', 'page-filter-input');
+          pageFilterLabel.setAttribute('class', 'filter-label');
+          pageFilterLabel.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M7 11.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>
+            </svg>`;
+
+          const pageFilterInput = document.createElement('input');
+          pageFilterInput.setAttribute('type', 'text');
+          pageFilterInput.setAttribute('id', 'page-filter-input');
+          pageFilterInput.setAttribute('class', 'filter-input');
+          pageFilterInput.setAttribute('placeholder', 'Filter by title and filename, use comma to separate');
+          pageFilterInput.setAttribute('autocomplete', 'off');
+          pageFilterInput.setAttribute('autocorrect', 'off');
+          pageFilterInput.setAttribute('autocapitalize', 'off');
+          pageFilterInput.setAttribute('spellcheck', 'false');
+          pageFilterInput.addEventListener('input', function (e) {
+            const filter = e.target.value.toLowerCase();
+            const words = filter.split(',');
+            for (let i = 0; i < words.length; i++) {
+              words[i] = words[i].trim();
+            }
+            state.pageCards.forEach(function (pageCard) {
+              pageCard.dispatchEvent(new CustomEvent("filterPageCards", {detail: {words: words}}));
+            });
+          });
+
+          pageFilter.appendChild(pageFilterLabel);
+          pageFilter.appendChild(pageFilterInput);
+
+          // page navigation
           const tagNavigation = document.createElement('div');
           tagNavigation.setAttribute('class', 'tag-navigation');
 
@@ -518,6 +646,24 @@
               <a href="${page.file}" class="page-card-title">${page.title}</a>
               <a href="${page.file}" class="page-card-filename">${page.file}</a>
             `;
+            pageCard.addEventListener('filterPageCards', function (e) {
+              console.log(e.detail.words);
+              if (e.detail.words.length === 0) {
+                pageCard.classList.remove('filter-hidden');
+              } else {
+                let show = false;
+                e.detail.words.forEach(function (word) {
+                  if (page.title.indexOf(word) !== -1 || page.file.indexOf(word) !== -1) {
+                    show = true;
+                  }
+                });
+                if (show) {
+                  pageCard.classList.remove('filter-hidden');
+                } else {
+                  pageCard.classList.add('filter-hidden');
+                }
+              }
+            }, false);
 
             if (page.tags && Array.isArray(page.tags) && page.tags.length > 0) {
 
@@ -557,22 +703,25 @@
               });
               pageCard.appendChild(tags);
             }
-
+            state.pageCards.push(pageCard);
             tagNavigation.appendChild(pageCard);
           });
+
+          // put it all together
+          const tagCloudDetails = document.createElement('details');
+          tagCloudDetails.setAttribute('class', 'tag-cloud-details');
+          tagCloudDetails.setAttribute('open', 'open');
+          tagCloudDetails.innerHTML = `<summary>Tags</summary>`;
+
+          tagCloudDetails.appendChild(tagCloudHeader);
+          tagCloudDetails.appendChild(tagCloud);
 
           const tagNavigationDetails = document.createElement('details');
           tagNavigationDetails.setAttribute('class', 'tag-navigation-details');
           tagNavigationDetails.setAttribute('open', 'open');
           tagNavigationDetails.innerHTML = `<summary>Pages</summary>`;
+          tagNavigationDetails.appendChild(pageFilter);
           tagNavigationDetails.appendChild(tagNavigation);
-
-          const tagCloudDetails = document.createElement('details');
-          tagCloudDetails.setAttribute('class', 'tag-cloud-details');
-          tagCloudDetails.setAttribute('open', 'open');
-          tagCloudDetails.innerHTML = `<summary>Tags</summary>`;
-          tagCloudDetails.appendChild(tagCloudFilter);
-          tagCloudDetails.appendChild(tagCloud);
 
           modalBodyContent.appendChild(tagCloudDetails);
           modalBodyContent.appendChild(tagNavigationDetails);
