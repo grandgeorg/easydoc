3cda84996b00657abee26b0db17a8c1ab41fa0b7
grandgeorg <viktor@grandgeorg.de>
2022-11-07 17:00:54 +0100

    add meta

---
+++ www/meta.js
@@ -0,0 +1,5 @@
+const easydocMeta = {
+      t: {"en":{"mtime":"Last Update","toc":"Table of Contents","nav":"Navigation","tagNav":"Tag Navigator"},"de":{"mtime":"letzte Aktualisierung","toc":"Inhaltsverzeichnis","nav":"Navigation","tagNav":"Tag Navigator"}},
+      pages: [{"title":"My Title","lang":"en","date":"2022-11-07T15:59:54.935Z","file":"index.html","tags":[]}],
+      tags: []
+    };
