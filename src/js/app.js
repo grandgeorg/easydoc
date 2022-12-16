0024af4866cdf88eb3d93651e5b0cdc7a2dcee16
grandgeorg <viktor@grandgeorg.de>
2022-12-16 17:37:18 +0100

    add sort by date

--- src/js/app.js
+++ src/js/app.js
@@ -126,6 +126,22 @@
       }
     }
 
+    function formatDate (dateString, lang) {
+      const date = new Date(dateString);
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+
+      if(lang === "de") {
+        return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+      } else {
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      }
+    }
+
     function dispatchNavigation() {
       if (!elMenuToggle) {
         return;
@@ -586,6 +602,30 @@
       });
     }
 
+    function sortPageCards(sortby, order, a, b) {
+      if (sortby === "title") {
+        if (order === "asc") {
+          return a.title.localeCompare(b.title);
+        } else {
+          return b.title.localeCompare(a.title);
+        }
+      } else if (sortby === "file") {
+        if (order === "asc") {
+          return a.file.localeCompare(b.file);
+        } else {
+          return b.file.localeCompare(a.file);
+        }
+      } else if (sortby === "date") {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        if (order === "asc") {
+          return dateA - dateB;
+        } else {
+          return dateB - dateA;
+        }
+      }
+    }
+
     // tag navigation
     function registerTagNavigation() {
       const openButton = document.querySelector("#open-tag-navigation");
@@ -629,6 +669,15 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
               <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+            </svg>`,
+          globe: `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe2" viewBox="0 0 16 16">
+              <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855-.143.268-.276.56-.395.872.705.157 1.472.257 2.282.287V1.077zM4.249 3.539c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.312.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a13.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.694.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.418.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z"/>
+            </svg>`,
+          time: `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+              <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
             </svg>`,
         };
 
@@ -929,21 +978,14 @@
           tagNavigationSortSelect.setAttribute("class", "sort-select");
           tagNavigationSortSelect.innerHTML = `
             <option value="title">${state.meta.t[state.global.lang].title}</option>
-            <option value="file">${state.meta.t[state.global.lang].file}</option>`;
+            <option value="file">${state.meta.t[state.global.lang].file}</option>
+            <option value="date">${state.meta.t[state.global.lang].date}</option>`;
           tagNavigationSortSelect.value = state.tagPages.sortby;
           tagNavigationSortSelect.addEventListener("change", (event) => {
             const sortValue = event.target.value;
             updateTagNavigationState({ sortby: sortValue });
             state.meta.pages.sort(function (a, b) {
-              if (sortValue === "title" && tagNavigationOrderSelect.value === "asc") {
-                return a.title.localeCompare(b.title);
-              } else if (sortValue === "title" && tagNavigationOrderSelect.value === "desc") {
-                return b.title.localeCompare(a.title);
-              } else if (sortValue === "file" && tagNavigationOrderSelect.value === "asc") {
-                return a.file.localeCompare(b.file);
-              } else if (sortValue === "file" && tagNavigationOrderSelect.value === "desc") {
-                return b.file.localeCompare(a.file);
-              }
+              return sortPageCards(sortValue, tagNavigationOrderSelect.value, a, b);
             });
             // update page cards a.k.a. tag navigation
             tagNavigationDetails.removeChild(tagNavigation);
@@ -974,15 +1016,7 @@
             const orderValue = event.target.value;
             updateTagNavigationState({ order: orderValue });
             state.meta.pages.sort(function (a, b) {
-              if (tagNavigationSortSelect.value === "title" && orderValue === "asc") {
-                return a.title.localeCompare(b.title);
-              } else if (tagNavigationSortSelect.value === "title" && orderValue === "desc") {
-                return b.title.localeCompare(a.title);
-              } else if (tagNavigationSortSelect.value === "file" && orderValue === "asc") {
-                return a.file.localeCompare(b.file);
-              } else if (tagNavigationSortSelect.value === "file" && orderValue === "desc") {
-                return b.file.localeCompare(a.file);
-              }
+              return sortPageCards(tagNavigationSortSelect.value, orderValue, a, b);
             });
             // update page cards a.k.a. tag navigation
             tagNavigationDetails.removeChild(tagNavigation);
@@ -1010,15 +1044,7 @@
 
           // page cards navigation a.k.a. tag navigation
           state.meta.pages.sort(function (a, b) {
-            if (state.tagPages.sortby === "title" && state.tagPages.order === "asc") {
-              return a.title.localeCompare(b.title);
-            } else if (state.tagPages.sortby === "title" && state.tagPages.order === "desc") {
-              return b.title.localeCompare(a.title);
-            } else if (state.tagPages.sortby === "file" && state.tagPages.order === "asc") {
-              return a.file.localeCompare(b.file);
-            } else if (state.tagPages.sortby === "file" && state.tagPages.order === "desc") {
-              return b.file.localeCompare(a.file);
-            }
+            return sortPageCards(state.tagPages.sortby, state.tagPages.order, a, b);
           });
           let tagNavigation = getTagNavigation();
 
@@ -1028,6 +1054,7 @@
 
             state.meta.pages.forEach((page) => {
               const pageCard = document.createElement("div");
+              const fileDate = formatDate(page.date, state.global.lang);
               if (state.global.filename === page.file) {
                 pageCard.setAttribute("class", "page-card current");
               } else {
@@ -1035,8 +1062,14 @@
               }
               pageCard.setAttribute("id", "page-" + page.file);
               pageCard.innerHTML = `
-                <a href="${page.file}" class="page-card-title">${page.title}</a>
-                <a href="${page.file}" class="page-card-filename">${page.file}</a>
+                <a href="${page.file}">
+                  <div class="page-card-title">${page.title}</div>
+                  <div class="page-card-filename">${page.file}</div>
+                  <div class="page-card-date">
+                    ${icons.time}${fileDate}
+                    ${icons.globe}${page.lang.toUpperCase()}
+                  </div>
+                </a>
               `;
               pageCard.addEventListener(
                 "filterPageCards",
@@ -1295,7 +1328,6 @@
         header.appendChild(title);
         content.appendChild(header);
 
-        // cookieConsent.appendChild(closeButton);
         body.appendChild(cookieConsent);
         content.appendChild(body);
 
@@ -1310,14 +1342,6 @@
         setTimeout(() => {
           closeButton.focus();
         }, 100);
-
-
-        // addModal(dialog, false);
-
-        // addModalDialog(
-        //   cookieConsentBody,
-        //   state.meta.t[state.global.lang].cookie_consent_title,
-        // );
       }
     }
 
